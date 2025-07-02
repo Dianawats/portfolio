45d797cf4267e7c47f72b627598db163fc6495c4
Dianawats <dianakiznaki@gmail.com>
2025-07-02 17:32:24 +0300

    update hero file

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -51,7 +51,7 @@ const Hero = () => {
             </div>
 
             <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
-              Hi, I’m Didi, a developer based in Croatia with a passion for
+              Hi, I’m Didi Genius, a developer based in Uganda with a passion for
               code.
             </p>
 
