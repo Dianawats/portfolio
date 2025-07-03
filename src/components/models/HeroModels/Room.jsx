f6c46819b9d3af485dd651aa7869f79eb475af81
Dianawats <dianakiznaki@gmail.com>
2025-07-03 09:18:55 +0300

    update files

--- src/components/models/HeroModels/Room.jsx
+++ src/components/models/HeroModels/Room.jsx
@@ -6,6 +6,7 @@ import * as THREE from "three";
 
 export function Room(props) {
   const { nodes, materials } = useGLTF("/models/optimized-room.glb");
+
   const screensRef = useRef();
   const matcapTexture = useTexture("/images/textures/mat1.png");
 
